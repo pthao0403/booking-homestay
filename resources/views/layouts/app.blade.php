317ab92d3af149a47c4c0df7d14a4c8d86454efc
pthao0403 <phuongthao05092021@gmail.com>
2026-06-23 02:44:03 +0700

    Update layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    </head>
+    <body class="font-sans antialiased">
 </head>
 <body>
 
