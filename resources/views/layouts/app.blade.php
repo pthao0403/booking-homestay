23ce62b5d845673241ac49680a714f575a2043bb
pthao0403 <phuongthao05092021@gmail.com>
2026-06-23 20:29:42 +0700

    Fix merge conflicts in app.blade.php - remove duplicate closing tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     @stack('scripts')
-</div>
 
         <script type="text/javascript">
             function googleTranslateElementInit() {
@@ -65,7 +64,5 @@
         </script>
         <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     </body>
-</html>
-</body>
 </html>
 
